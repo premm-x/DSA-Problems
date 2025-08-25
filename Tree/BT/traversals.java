c03cd402f57dd7b784f213e3e1ad212e0607f027
prem sharma <mybrother1370@gmail.com>
2025-08-25 20:53:22 +0530

    Create traversals.java

---
+++ Tree/BT/traversals.java
@@ -0,0 +1,20 @@
+// pre-order -- (recursion / iteration)
+
+
+// in-order -- (recursion / iteration)
+
+
+// post-order -- (recursion / iteration)
+
+
+// level-order -- (recursion / iteration)
+
+
+// zig-zag-order -- (recursion / iteration)
+
+
+// boundry-order -- (recursion / iteration)
+
+
+// vertical-order -- (recursion / iteration)
+
