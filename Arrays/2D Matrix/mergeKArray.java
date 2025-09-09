fd6e7bebfff9ffc70f6ec8cfe7bd40d6b6bb319e
prem sharma <mybrother1370@gmail.com>
2025-09-09 20:31:48 +0530

    Create mergeKArray.java

---
+++ Arrays/2D Matrix/mergeKArray.java
@@ -0,0 +1,18 @@
+// Brute --- tc O(n log n) - sc O(n) --- Queue
+
+public static ArrayList<Integer> mergeKArrays(int[][] arr, int K) {
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                q.offer(arr[i][j]);
+            }
+        }
+        while(!q.isEmpty()) ans.add(q.poll());
+        
+        return ans;
+    }
+
+//-----------------------------------------------------
