c920065eb9437133e07341a519902eb76a6dcbfa
prem sharma <mybrother1370@gmail.com>
2025-09-14 22:09:00 +0530

    Update medianTwoSortedArrays.java

--- Arrays/1D array/medianTwoSortedArrays.java
+++ Arrays/1D array/medianTwoSortedArrays.java
@@ -1,4 +1,8 @@
-// brute --- O(N logn)
+// 1. brute --- O(N logn)
+// 2. optimal --- O(log (m+n)
+
+//---------------------------------------------------------
+// 1. brute --- O(N logn)
 
 public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
@@ -22,3 +26,76 @@ public double findMedianSortedArrays(int[] nums1, int[] nums2) {
             return (double) arr[n/2];
 
     }
+
+
+// 2. optimal --- O(log (m+n)
+
+public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int n = nums1.length + nums2.length;
+        int trg = (int)Math.ceil(n/2.0) , count = 0;
+
+        int p1 = 0, p2 = 0;
+
+        while(p1 < nums1.length && p2 < nums2.length){
+            if(nums1[p1] < nums2[p2]){
+                p1++;
+                count++;
+                if(count == trg) {
+                    if(n%2 == 0){
+                        if(nums1.length - p1 == 0)
+                            return (nums1[p1-1] + nums2[p2]) / 2.0;
+                        else{
+                            return (nums1[p1-1] + Math.min(nums1[p1], nums2[p2])) / 2.0;
+                        }
+                    }
+                    else
+                        return (double) nums1[p1-1];
+                }
+            }
+            else{
+                p2++;
+                count++;
+                if(count == trg) {
+                    if(n%2 == 0){
+                        if(nums2.length - p2 == 0)
+                            return (nums2[p2-1] + nums1[p1]) / 2.0;
+                        else{
+                            return (nums2[p2-1] + Math.min(nums1[p1], nums2[p2])) / 2.0;
+                        }
+                    }
+                    else
+                        return (double) nums2[p2-1];
+                }
+            }
+        }
+
+        if(count != trg){
+            if(p1 < nums1.length){
+                p1 = p1 + (trg - count)-1;
+                
+                if(n%2 == 0){
+                    return (nums1[p1] + nums1[p1+1]) / 2.0;
+                }
+                else
+                    return (double) nums1[p1];
+            }
+
+            else{
+                p2 = p2 + (trg - count)-1;
+
+                if(n%2 == 0){
+                    return (nums2[p2] + nums2[p2+1]) / 2.0;
+                }
+                else
+                    return (double) nums2[p2];
+            }
+        }
+
+        return -1;
+    }
+
+
+
+
+
