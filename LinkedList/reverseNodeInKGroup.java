dc20ed4f47b2fea1900bf933bef34af7994e9aa8
prem sharma <mybrother1370@gmail.com>
2025-10-07 19:30:19 +0530

    Update reverseNodeInKGroup.java

--- LinkedList/reverseNodeInKGroup.java
+++ LinkedList/reverseNodeInKGroup.java
@@ -30,5 +30,35 @@ class Solution {
 
 // ------------------------------ ( 2 ) ----------------------------------
 // optimal --- tc: O(n) sc: O(1)
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
 
+        if(head == null || head.next == null ) return head;
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy, curr = head;
+
+        int count = 0;
+        while(curr != null){
+            count++;
+            curr = curr.next;
+        }
+
+        while(count >= k){
+            curr = prev.next;
+            ListNode next = curr.next;
+
+            for(int i=1; i<k; i++){
+                curr.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+                next = curr.next;
+            }
+            prev = curr;
+            count = count - k;
+        }
+        return dummy.next;
+    }
+}
 
