cd896aa1bdf46294f74e6a092463631868ff5bc3
prem sharma <mybrother1370@gmail.com>
2025-10-09 21:32:36 +0530

    Create odd&even.java

---
+++ LinkedList/odd&even.java
@@ -0,0 +1,24 @@
+// solution - 1
+
+// two pointer --- tc: O(n) sc: O(1)
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        
+        if(head == null || head.next == null ) return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+
+        while(even != null && even.next != null){
+
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
