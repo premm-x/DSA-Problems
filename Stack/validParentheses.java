1358667fffc789fbd9d93ab009a557b64c5d0a51
prem sharma <mybrother1370@gmail.com>
2025-10-13 20:42:49 +0530

    Create validParentheses.java

---
+++ Stack/validParentheses.java
@@ -0,0 +1,27 @@
+// solution - 1
+
+// stack --- tc: O(n) sc: O(1)
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char letter = s.charAt(i);
+
+            if(letter == '(' || letter == '[' || letter == '{'){
+                st.push(letter);
+            }
+            else{
+                
+                if(st.isEmpty()) return false;
+
+                char top = st.peek();
+                if(top == '(' && letter == ')' || top == '[' && letter == ']' || top == '{' && letter == '}'){
+                    st.pop();
+                }  
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
