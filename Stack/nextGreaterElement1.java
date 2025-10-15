05ef47e6559ec6174d946a8d160625e4baab1304
prem sharma <mybrother1370@gmail.com>
2025-10-15 19:55:29 +0530

    Update nextGreaterElement1.java

--- Stack/nextGreaterElement1.java
+++ Stack/nextGreaterElement1.java
@@ -1,4 +1,4 @@
-// solution - 1
+// solution - 2
 
 // brute force --- tc: O(n²) sc: O(nums1)
 // to optimize the sc use nums1 rather ans array
@@ -26,5 +26,31 @@ class Solution {
             open = false;
         }
         return ans;
+    }
+}
+
+// ---------------------------- ( 2 ) -------------------------------
+// optimazie ( hashMap + stack) --- tc: O(n) sc: O(n)
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<nums2.length; i++){
+
+            while(!st.isEmpty() && nums2[i] > st.peek()){
+                map.put(st.pop(), nums2[i]);
+            }
+
+            st.push(nums2[i]);
+        }
+
+        for(int i=0; i<nums1.length; i++){
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return nums1;
     }
 }
