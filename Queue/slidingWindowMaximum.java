d4a5b3c659792921a14897ac35c43b1f800e8268
prem sharma <mybrother1370@gmail.com>
2025-11-03 21:05:30 +0530

    Create slidingWindowMaximum.java

---
+++ Queue/slidingWindowMaximum.java
@@ -0,0 +1,20 @@
+// solution - 1
+
+// Deque approch --- tc: O(n) sc: (n)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+        int ans[] = new int[nums.length - (k-1)];
+        Deque<Integer> pq = new ArrayDeque<>();
+        int j = 0;
+
+        for(int i=0; i<nums.length; i++){
+            while(!pq.isEmpty() && pq.peekFirst() <= i-k) pq.pollFirst();
+            while(!pq.isEmpty() && nums[pq.peekLast()] < nums[i]) pq.pollLast();
+            
+            pq.offerLast(i);
+            if(i >= k-1) ans[j++] = nums[pq.peekFirst()];
+        }
+        return ans;
+    }
+}
