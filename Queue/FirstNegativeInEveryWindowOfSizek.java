2015a6eb5ffcf181afb2abc146f6f160f66ffcd9
prem sharma <mybrother1370@gmail.com>
2025-11-04 20:06:04 +0530

    Create FirstNegativeInEveryWindowOfSizek.java

---
+++ Queue/FirstNegativeInEveryWindowOfSizek.java
@@ -0,0 +1,23 @@
+// solution -1
+
+// sliding Window max approch used i here --- tc: O(n) sc: O(n)
+class Solution {
+    static List<Integer> firstNegInt(int arr[], int k) {
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> ans = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++){
+            
+            while(!dq.isEmpty() && dq.peekFirst() <= i-k) dq.pollFirst();
+            
+            if(arr[i] < 0) dq.offerLast(i);
+            
+            if(i >= k-1){
+               if(dq.isEmpty()) ans.add(0);
+               else ans.add(arr[dq.peekFirst()]);
+            }
+        }
+        return ans;
+    }
+}
