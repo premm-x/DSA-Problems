6f14490622e7e93df1d90a318c4cf6579cd05976
prem sharma <mybrother1370@gmail.com>
2025-11-13 18:43:51 +0530

    Update interLeaveTheFirst&SecondHalfOfQueue.java

--- Queue/interLeaveTheFirst&SecondHalfOfQueue.java
+++ Queue/interLeaveTheFirst&SecondHalfOfQueue.java
@@ -1,4 +1,4 @@
-// solution - 1
+// solution - 2
 
 // brute force --- tc: O(n) sc: O(n)
 class Solution {
@@ -23,3 +23,24 @@ class Solution {
         }
     }
 }
+
+// ------------------------------- ( 2 ) -----------------------------------
+// array based answer --- tc: O(n) sc: O(n)
+class Solution {
+    public void rearrangeQueue(Queue<Integer> q) {
+        
+        int n = q.size();
+        int arr[] = new int[n];
+        
+        for(int i=0; i<n; i++){
+            arr[i] = q.poll();
+        }
+        
+        for(int i=0; i<n/2; i++){
+            q.offer(arr[i]);
+            q.offer(arr[(n/2)+i]);
+        }
+    }
+}
+
+
