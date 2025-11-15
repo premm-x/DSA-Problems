289d1e191646be16c18bacabf80facf04d25a8f9
prem sharma <mybrother1370@gmail.com>
2025-11-15 22:12:44 +0530

    Update CoreTreeUtilitiesProblem.java

--- Tree/BT/CoreTreeUtilitiesProblem.java
+++ Tree/BT/CoreTreeUtilitiesProblem.java
@@ -1,15 +1,27 @@
-// height
 
-
+//---------------- ( remaiming ) ------------------
 // balance
-
-
 // diameter
 
+//---------------- ( completed ) ------------------
+// height
+// count all node
+// count Leaf Node
+//-------------------------------------------------
 
+int height(Node root){
+    if(root == null) return 0;
+    return Math.max(height(root.left), height(root.right)) + 1;
+}
+        
+int count(Node root){
+    if(root == null) return 0;
+    return count(root.left) + count(root.right) + 1;
+}
 
-// ------------------------------------------------------------------------------------
-// only questions names (diff folders questions)
-
-// 1. maximum path sum
+int countLeafNode(Node root){
+    if(root == null) return 0;
+    if(root.left == null && root.right == null) return 1;
+    return countLeafNode(root.left) + countLeafNode(root.right);
+}
 
