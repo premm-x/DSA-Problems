7a15a0d2c3c55ffda71d72c902b34b53f356aeaa
prem sharma <mybrother1370@gmail.com>
2025-11-19 16:17:17 +0530

    Update CoreUtilFunction.java

--- Tree/BST/CoreUtilFunction.java
+++ Tree/BST/CoreUtilFunction.java
@@ -1,12 +1,12 @@
 
 //--------------- ( remaining ) --------------
-// insert ( iterative )
-// delete ( recursive / iterative )
+// delete ( iterative )
 //--------------------------------------------
 
 //--------------- ( completed ) --------------
 // search ( recursive / iterative )
-// insert ( recursive )
+// insert ( recursive / iterative )
+// delete ( recursive )
 //--------------------------------------------
 
 static class BinarySearchTree{
@@ -28,6 +28,22 @@ static class BinarySearchTree{
             else if (target < root.val) return search(root.left, target);
             else return search(root.left, target);
         }
+        
+        // insert ( iterative )
+        void inseart(Node root, int val){
+            if(root == null) return;
+            Node node = null;
+
+            while (root != null) {
+                node = root;
+                if(val < root.val) root = root.left;
+                else if(val > root.val) root = root.right;
+                else return;
+            }
+
+            if(val < node.val && node.left == null) node.left = new Node(val);
+            else node.right = new Node(val);
+        }
 
         // insert ( recursive )
         Node insert(Node root, int val){
@@ -38,5 +54,27 @@ static class BinarySearchTree{
 
             return root;
         }
+        
+        // delete ( recursive )
+        Node delete(Node root, int val){
+            if(root == null) return null;
+            
+            if(val < root.val) root.left = delete(root.left, val);
+            else if(val > root.val) root.right = delete(root.right, val);
+            else{
+
+                if(root.left == null) return root.right;
+                if(root.right == null) return root.left;
+
+                Node node = root.right;
+                while(node.left != null) node = node.left;
+                root.val = node.val;
+
+                root.right = delete(root.right, node.val);
+                
+            }
+            return root;
+        }
+
 
     }
