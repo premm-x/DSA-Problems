0d5498397950fb565a089c5a098aeb80cd52445a
prem sharma <mybrother1370@gmail.com>
2026-01-04 20:54:06 +0530

    Update bridge_in_graph.java

--- Graph/bridge_in_graph.java
+++ Graph/bridge_in_graph.java
@@ -1,4 +1,6 @@
-// solution - 1
+// solution - 2
+
+//  brute - optimal
 
 //  ------------------------------------ ( 1 ) -------------------------------------
 // brute approch ( remove every edges temporarily and then check ) --- tc : O(E * (V+E)) sc : O(V)
@@ -41,8 +43,57 @@ void bridge(List<List<Integer>> graph, int V){
 
 
 //  ------------------------------------ ( 2 ) -------------------------------------
-// brute approch ( remove every edges temporarily and then check ) --- tc : O(E * (V+E)) sc : O(V)
+// optima approch ( tarjan algo ) ( used discovey and lowest times ) --- tc : O(V+E) sc : O(V)
+class Solution {
+    int[] time, lowestTime;
+    int ttime;
+    boolean[] visited;
+    List<List<Integer>> bridges = new ArrayList<>();
+    
+    void dfs(List<List<Integer>> graph, int V, int src, int parent){
+        visited[src] = true;
+        
+        time[src] = lowestTime[src] = ttime++;
+        
+        for(int v : graph.get(src)){
+            if(v == parent) continue;
+            
+            if(!visited[v]){
+                dfs(graph, V, v, src);
+                
+                lowestTime[src] = Math.min(lowestTime[v], lowestTime[src]);
+                
+                if(time[src] < lowestTime[v]){
+                    bridges.add(new ArrayList<>(List.of(src, v)));
+                }
+            }
+            else{
+                lowestTime[src] = Math.min(lowestTime[src], lowestTime[v]);
+            }
+        }
+    }
 
+    public List<List<Integer>> criticalConnections(int V, List<List<Integer>> edges) {
+        
+        List<List<Integer>> graph = new ArrayList<>();
+        
+        for(int i=0; i<V; i++) graph.add(new ArrayList<>());
+        
+        for(List<Integer> e: edges){
+            graph.get(e.get(0)).add(e.get(1));
+            graph.get(e.get(1)).add(e.get(0));
+        }
+
+        time = new int[V];
+        lowestTime = new int[V];
+        ttime = 0;
+        visited = new boolean[V];
+        
+        dfs(graph, V, 0, -1);
+        
+        return bridges;
+    }
+}
 
 
 
